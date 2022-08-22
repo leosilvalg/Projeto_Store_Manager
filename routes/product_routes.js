f16540a9911a339a9a119230f53f0dbe9fc2c2f0
Leonardo Silva <leonardorc.geo@gmail.com>
2022-08-22 19:56:49 -0300

    Requisito 04. Validando o campo de nome

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const productController = require('../controllers/product_controller');
+const isNameValid = require('../middlewares/productValidation');
 
 const route = express.Router();
 
 route.get('/', productController.getAll);
 route.get('/:id', productController.getById);
-route.post('/', productController.create);
+route.post('/', isNameValid, productController.create);
 route.put('/:id', productController.update);
 route.post('/:id', productController.deleted);
 
