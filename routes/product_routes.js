f33374a4a88e0a042e2f207eb24fcfe1e65fa528
Leonardo Silva <leonardorc.geo@gmail.com>
2022-08-22 22:31:42 -0300

    Requisito 10. Ele já estava pronto, porém, nao tinha colocado a validação do nome

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -7,7 +7,7 @@ const route = express.Router();
 route.get('/', productController.getAll);
 route.get('/:id', productController.getById);
 route.post('/', isNameValid, productController.create);
-route.put('/:id', productController.update);
+route.put('/:id', isNameValid, productController.update);
 route.post('/:id', productController.deleted);
 
 module.exports = route;
